d5083a8684fac90eef1232d3cacbcaefc67bafd6
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-04 15:42:44 +0300

    update

--- script.js
+++ script.js
@@ -22,14 +22,3 @@ menuBtn.addEventListener("click", () => {
 });
 
 
-const hiddenElements = document.querySelectorAll(".hidden");
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-hiddenElements.forEach((el) => observer.observe(el));
