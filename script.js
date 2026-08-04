c12f729d5cbf54d15502ca319fd4070531818c92
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-04 11:44:32 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
-menuBtn.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-});
+// const menuBtn = document.querySelector(".menu-btn");
+// const navLinks = document.querySelector(".nav-links");
+// menuBtn.addEventListener("click", () => {
+//         navLinks.classList.toggle("active");
+// });
+
+
+
+
