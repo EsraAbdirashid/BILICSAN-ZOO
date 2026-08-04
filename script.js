cd5c8961d470fbad6842eac652f77e13e920b61a
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-04 11:44:53 +0300

    update

--- script.js
+++ script.js
@@ -5,5 +5,19 @@
 // });
 
 
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+const menuIcon = document.querySelector(".menu-btn i");
 
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+});
 
